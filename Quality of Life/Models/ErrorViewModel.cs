0b89448b214887e682986fa915ab4a2809623695
Hasheem Ditano <aoditano@student.apc.edu.ph>
2025-11-28 02:12:38 +0800

    Delete Quality of Life directory

--- Quality of Life/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Quality_of_Life.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
