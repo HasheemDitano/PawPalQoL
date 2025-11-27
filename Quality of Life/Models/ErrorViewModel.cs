1a171342250521923018d19b5d362b774ddb102e
Hasheem Ditano <aoditano@student.apc.edu.com>
2025-11-27 19:40:33 +0800

    Merge changes and add project files with proper .gitignore

---
+++ Quality of Life/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Quality_of_Life.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Quality of Life/Models/QualityOfLifeModel.cs
@@ -0,0 +1,20 @@
+﻿namespace PawPalQoL.Models
+{
+    public class QualityOfLifeModel
+    {
+        // User input properties
+        public int Pain { get; set; }
+        public int Mobility { get; set; }
+        public int Appetite { get; set; }
+        public int Happiness { get; set; }
+
+        // Calculated result
+        public int TotalScore
+        {
+            get
+            {
+                return Pain + Mobility + Appetite + Happiness;
+            }
+        }
+    }
+}
